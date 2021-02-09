18e589df58c9a3b721f82317de776c623365c833
hernanRios <hernanriosvalencia92@gmail.com>
2021-02-09 02:27:30 -0500

    First commit

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 module.exports={
 
     database:{
-        uri:'mongodb://"mongodb+srv://cluster0.xs7ry.mongodb.net/<crabbitSchool>" --username crabbitSchool'
+        uri:'mongo "mongodb+srv://cluster0.xs7ry.mongodb.net/<crabbitSchool>" --username crabbitSchool'
     }
 
 }
